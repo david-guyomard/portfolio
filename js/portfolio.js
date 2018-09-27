d232a652764485ad3b492d17689281242fadd88a
david <david.guyomard@live.fr>
2018-09-27 15:12:07 +0200

    initial commit

---
+++ js/portfolio.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#contact").click(function(){
+        $("#modalShow").modal('show');
+    });
+    $("#cvLink").click(function(){
+        $("#modalCv").modal('show')
+    });
+});
