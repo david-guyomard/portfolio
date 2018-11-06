0dc779dab0e46d88f334a32e701f21cba4a88686
David <david.guyomard@live.fr>
2018-11-06 13:57:58 +0100

    new folio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    window.alert('En venant sur ce site vous acceptez les cookies !')
     $("#contact").click(function () {
         $("#modalShow").modal('show');
     });
@@ -11,17 +12,5 @@ $(document).ready(function () {
     $(".parallax-window").parallax({
         naturalWidth: 600,
         naturalHeight: 400
-      });
-    /*$("#competences, #mesProjets, #mesExperiences").parallax({
-        image: "parallax/demo/images/samples/2.jpg",
-        height: "400px",
-        scroll: "manual"
     });
-    /*$(document).scroll(function () {
-
-        var scrollPosition = $(this).scrollTop();
-
-        $("#competences, #mesProjets, #mesExperiences").parallax <a href= "https://www.jqueryscript.net/tags.php?/Scroll/"> Scroll</a> (scrollPosition);
-
-    });*/
 });
